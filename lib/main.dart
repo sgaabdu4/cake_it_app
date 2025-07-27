3f79249628f3781bfd85fd3b0a69f0bf6acf6dfc
sgaabdu4 <abdulgafoorabid@gmail.com>
2025-07-27 04:23:04 +0100

    Refactor app structure: Move MyApp class from app.dart to main.dart and update imports for improved organization.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:cake_it_app/core/app_routes.dart';
+import 'package:cake_it_app/core/route_generator.dart';
+import 'package:cake_it_app/localization/app_localizations.dart';
 import 'package:flutter/material.dart';
 
-import 'app.dart';
 import 'features/settings/presentation/controllers/settings_controller.dart';
 
 void main() async {
@@ -11,4 +13,37 @@ void main() async {
   await settingsController.loadSettings();
 
   runApp(MyApp(settingsController: settingsController));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+    required this.settingsController,
+  });
+
+  final SettingsController settingsController;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: settingsController,
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          restorationScopeId: 'app',
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          locale: settingsController.locale,
+          onGenerateTitle: (BuildContext context) =>
+              AppLocalizations.of(context)!.appTitle,
+          theme: ThemeData(),
+          darkTheme: ThemeData.dark(),
+          themeMode: settingsController.themeMode,
+          initialRoute: AppRoutes.home,
+          onGenerateRoute: (settings) =>
+              RouteGenerator.generateRoute(settings, settingsController),
+        );
+      },
+    );
+  }
 }
